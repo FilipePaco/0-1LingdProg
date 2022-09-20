85331efbe6f01d4f4b493884df576a22697be4ae
FilipePaco <102427696+FilipePaco@users.noreply.github.com>
2022-09-20 19:22:42 -0300

    Update myApp.js

--- Backendfreecodecamp/2NodeJs/boilerplate-express-main/myApp.js
+++ Backendfreecodecamp/2NodeJs/boilerplate-express-main/myApp.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let app = express();
 
+  app.get('/',function(req,res)
+  {
+    res.send('Hello World!');
+  });
 
-
-
+  app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+// listen inicia um servidor, e colocamos ele para escutar a porta 3000. Ele responde com "Hello World!"
 
 
 
